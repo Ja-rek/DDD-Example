3cf06d918bbe5386a87210ba4cca2d28fa785473
Ja-Rek <dto.jarek@gmail.com>
2024-07-22 17:59:43 +0200

    Add invoice domain model

---
+++ Domain/Invoices/Invoice.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Invoices;
+
+public class Invoice(Guid clientId)
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public Guid ClientId { get;} = clientId != default 
+        ? clientId 
+        : throw new ArgumentException($"{clientId} must to have a value.");
+    public DateTime CreationDate { get; } = DateTime.UtcNow;
+    public List<InvoiceItem> Items { get; } = new List<InvoiceItem>();
+
+    public void AddItem(InvoiceItem item)
+    {
+        if (item is null)
+        {
+            throw new ArgumentNullException(nameof(item));
+        }
+
+        Items.Add(item);
+    }
+}
---
+++ Domain/Invoices/InvoiceItem.cs
@@ -0,0 +1,36 @@
+﻿namespace Domain.Invoices;
+
+public record InvoiceItem
+{
+    public InvoiceItem(Guid serviceId, DateTime startDate, DateTime endDate, decimal value, bool isPaused)
+    {
+        ServiceId = serviceId != default 
+            ? serviceId 
+            : throw new ArgumentException($"{nameof(ServiceId)} must have a value.", nameof(serviceId));
+
+        StartDate = startDate != default 
+            ? startDate 
+            : throw new ArgumentException($"{nameof(StartDate)} must have a value.", nameof(startDate));
+
+        EndDate = endDate != default 
+            ? endDate 
+            : throw new ArgumentException($"{nameof(EndDate)} must have a value.", nameof(endDate));
+
+        if (startDate >= endDate)
+        {
+            throw new ArgumentException($"{nameof(startDate)} must be earlier than {nameof(endDate)}.");
+        }
+
+        Value = value > 0 
+            ? value 
+            : throw new ArgumentException($"{nameof(Value)} must be greater than zero.", nameof(value));
+
+        IsPaused = isPaused;
+    }
+
+    public DateTime StartDate { get; }
+    public DateTime EndDate { get; }
+    public decimal Value { get; }
+    public bool IsPaused { get; }
+    public Guid ServiceId { get; }
+}
