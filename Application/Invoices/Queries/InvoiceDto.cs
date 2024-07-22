a85523745dc3f470bc53484d97e449c1b7be2fc7
Ja-Rek <dto.jarek@gmail.com>
2024-07-22 23:12:52 +0200

    Add data formating

---
+++ Application/Invoices/Queries/InvoiceDto.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Invoices;
+using System.Globalization;
+
+namespace Application.Invoices.Queries;
+
+public record InvoiceDto(Invoice Invoice)
+{
+    public Guid Id { get; } = Invoice.Id;
+    public Guid ClientId { get; } = Invoice.ClientId;
+    public string CreationDate { get; } = Invoice.CreationDate.ToString(new CultureInfo("pl-PL"));
+    public IEnumerable<InvoiceItem> Items { get; } = Invoice.Items;
+}
---
+++ Application/Invoices/Queries/InvoiceQueryService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Invoices;
+
+namespace Application.Invoices.Queries;
+
+public class InvoiceQueryService(IInvoiceRepository invoiceRepository)
+{
+    private readonly IInvoiceRepository invoiceRepository = invoiceRepository;
+
+    public IEnumerable<InvoiceDto> GetInvoices() => invoiceRepository
+        .GetAllInvoices()
+        .Select(invoice => new InvoiceDto(invoice));
+}
