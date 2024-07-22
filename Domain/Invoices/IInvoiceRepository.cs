a0742f1d7e5196bf48d1162c51afdd9e5dcfec71
Ja-Rek <dto.jarek@gmail.com>
2024-07-22 18:03:34 +0200

    Add invoice repository

---
+++ Domain/Invoices/IInvoiceRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Invoices;
+
+public interface IInvoiceRepository
+{
+    void Save(Invoice invoice);
+    Invoice? GetInvoice(Guid clientId, int month, int year);
+    IEnumerable<Invoice> GetAllInvoices();
+}
---
+++ Infrastructure/InvoiceRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Invoices;
+
+namespace Infrastructure;
+
+public class InvoiceRepository : IInvoiceRepository
+{
+    private static readonly List<Invoice> _invoices = new List<Invoice>();
+
+    public void Save(Invoice invoice)
+    {
+        _invoices.Add(invoice);
+    }
+
+    public Invoice? GetInvoice(Guid clientId, int month, int year)
+    {
+        return _invoices.FirstOrDefault(inv => inv.ClientId == clientId && inv.CreationDate.Month == month && inv.CreationDate.Year == year);
+    }
+
+    public IEnumerable<Invoice> GetAllInvoices()
+    {
+        return _invoices;
+    }
+}
