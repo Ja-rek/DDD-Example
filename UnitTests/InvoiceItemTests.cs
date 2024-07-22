cefd0958823c127714e5dcc779e332dfe0f5e136
Ja-Rek <dto.jarek@gmail.com>
2024-07-22 18:01:14 +0200

    Add invoice tests

---
+++ UnitTests/InvoiceItemTests.cs
@@ -0,0 +1,114 @@
+﻿using Domain.Invoices;
+using FluentAssertions;
+
+namespace UnitTests;
+
+public class InvoiceItemTests
+{
+    [Test]
+    public void Constructor_ShouldThrowArgumentException_WhenServiceIdIsEmpty()
+    {
+        // Arrange
+        var serviceId = Guid.Empty;
+        var startDate = new DateTime(2023, 7, 18);
+        var endDate = new DateTime(2023, 7, 19);
+        var value = 100m;
+        var isPaused = false;
+
+        // Act
+        Action act = () => new InvoiceItem(serviceId, startDate, endDate, value, isPaused);
+
+        // Assert
+        act.Should().Throw<ArgumentException>().WithMessage("ServiceId must have a value.*");
+    }
+
+    [Test]
+    public void Constructor_ShouldThrowArgumentException_WhenStartDateIsDefault()
+    {
+        // Arrange
+        var serviceId = Guid.NewGuid();
+        var startDate = default(DateTime);
+        var endDate = new DateTime(2023, 7, 19);
+        var value = 100m;
+        var isPaused = false;
+
+        // Act
+        Action act = () => new InvoiceItem(serviceId, startDate, endDate, value, isPaused);
+
+        // Assert
+        act.Should().Throw<ArgumentException>().WithMessage("StartDate must have a value.*");
+    }
+
+    [Test]
+    public void Constructor_ShouldThrowArgumentException_WhenEndDateIsDefault()
+    {
+        // Arrange
+        var serviceId = Guid.NewGuid();
+        var startDate = new DateTime(2023, 7, 18);
+        var endDate = default(DateTime);
+        var value = 100m;
+        var isPaused = false;
+
+        // Act
+        Action act = () => new InvoiceItem(serviceId, startDate, endDate, value, isPaused);
+
+        // Assert
+        act.Should().Throw<ArgumentException>().WithMessage("EndDate must have a value.*");
+    }
+
+    [Test]
+    public void Constructor_ShouldThrowArgumentException_WhenStartDateIsGreaterThanOrEqualToEndDate()
+    {
+        // Arrange
+        var serviceId = Guid.NewGuid();
+        var startDate = new DateTime(2023, 7, 19);
+        var endDate = new DateTime(2023, 7, 18);
+        var value = 100m;
+        var isPaused = false;
+
+        // Act
+        Action act = () => new InvoiceItem(serviceId, startDate, endDate, value, isPaused);
+
+        // Assert
+        act.Should().Throw<ArgumentException>().WithMessage("startDate must be earlier than EndDate.");
+    }
+
+    [Test]
+    public void Constructor_ShouldThrowArgumentException_WhenValueIsLessThanOrEqualToZero()
+    {
+        // Arrange
+        var serviceId = Guid.NewGuid();
+        var startDate = new DateTime(2023, 7, 18);
+        var endDate = new DateTime(2023, 7, 19);
+        var value = 0m;
+        var isPaused = false;
+
+        // Act
+        Action act = () => new InvoiceItem(serviceId, startDate, endDate, value, isPaused);
+
+        // Assert
+        act.Should().Throw<ArgumentException>().WithMessage("Value must be greater than zero.*");
+    }
+
+    [Test]
+    public void Constructor_ShouldInitializeWithGivenValues()
+    {
+        // Arrange
+        var serviceId = Guid.NewGuid();
+        var startDate = new DateTime(2023, 7, 18);
+        var endDate = new DateTime(2023, 7, 19);
+        var value = 100m;
+        var isPaused = false;
+
+        // Act
+        var invoiceItem = new InvoiceItem(serviceId, startDate, endDate, value, isPaused);
+
+        // Assert
+        invoiceItem.ServiceId.Should().Be(serviceId);
+        invoiceItem.StartDate.Should().Be(startDate);
+        invoiceItem.EndDate.Should().Be(endDate);
+        invoiceItem.Value.Should().Be(value);
+        invoiceItem.IsPaused.Should().Be(isPaused);
+    }
+}
+
---
+++ UnitTests/InvoiceTests.cs
@@ -0,0 +1,109 @@
+﻿using FluentAssertions;
+using Domain.Invoices;
+
+namespace UnitTests;
+
+public class InvoiceTests
+{
+    [Test]
+    public void Constructor_ShouldThrowArgumentException_WhenClientIdIsEmpty()
+    {
+        // Arrange
+        var clientId = Guid.Empty;
+
+        // Act
+        Action act = () => new Invoice(clientId);
+
+        // Assert
+        act.Should().Throw<ArgumentException>().WithMessage($"{clientId} must to have a value.");
+    }
+
+    [Test]
+    public void Constructor_ShouldInitializeWithGivenClientId()
+    {
+        // Arrange
+        var clientId = Guid.NewGuid();
+
+        // Act
+        var invoice = new Invoice(clientId);
+
+        // Assert
+        invoice.ClientId.Should().Be(clientId);
+    }
+
+    [Test]
+    public void Constructor_ShouldInitializeWithNewGuidId()
+    {
+        // Arrange
+        var clientId = Guid.NewGuid();
+
+        // Act
+        var invoice = new Invoice(clientId);
+
+        // Assert
+        invoice.Id.Should().NotBeEmpty();
+    }
+
+    [Test]
+    public void Constructor_ShouldInitializeWithCurrentUtcDateTime()
+    {
+        // Arrange
+        var clientId = Guid.NewGuid();
+        var beforeCreation = DateTime.UtcNow;
+
+        // Act
+        var invoice = new Invoice(clientId);
+        var afterCreation = DateTime.UtcNow;
+
+        // Assert
+        invoice.CreationDate.Should().BeOnOrAfter(beforeCreation).And.BeOnOrBefore(afterCreation);
+    }
+
+    [Test]
+    public void Constructor_ShouldInitializeWithEmptyInvoiceItems()
+    {
+        // Arrange
+        var clientId = Guid.NewGuid();
+
+        // Act
+        var invoice = new Invoice(clientId);
+
+        // Assert
+        invoice.Items.Should().BeEmpty();
+    }
+
+    [Test]
+    public void AddItem_ShouldThrowArgumentNullException_WhenItemIsNull()
+    {
+        // Arrange
+        var clientId = Guid.NewGuid();
+        var invoice = new Invoice(clientId);
+
+        // Act
+        Action act = () => invoice.AddItem(null);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>().WithMessage("Value cannot be null. (Parameter 'item')");
+    }
+
+    [Test]
+    public void AddItem_ShouldAddInvoiceItemToItemsList()
+    {
+        // Arrange
+        var clientId = Guid.NewGuid();
+        var invoice = new Invoice(clientId);
+        var serviceId = Guid.NewGuid();
+        var startDate = new DateTime(2023, 7, 18);
+        var endDate = new DateTime(2023, 7, 19);
+        var value = 100m;
+        var isPaused = false;
+        var item = new InvoiceItem(serviceId, startDate, endDate, value, isPaused);
+
+        // Act
+        invoice.AddItem(item);
+
+        // Assert
+        invoice.Items.Should().ContainSingle(i => i == item);
+    }
+}
+
