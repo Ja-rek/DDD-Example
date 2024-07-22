5c0153387a3badcda6dd84c03713f7b621423d5f
Ja-Rek <dto.jarek@gmail.com>
2024-07-22 23:12:12 +0200

    Add autofac and endpoints

---
+++ Infrastructure/AutofacExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Application.Invoices.Queries;
+using Autofac;
+using Domain.Invoices;
+using Domain.Operations.Specyfications;
+using System.Reflection;
+
+namespace Infrastructure;
+
+public static class AutofacExtensions
+{
+    public static void RegisterServices(this ContainerBuilder builder)
+    {
+        string[] types = ["Repository", "Service", "Factory"];
+        Assembly[] assemblies =
+        [
+            typeof(InvoiceQueryService).Assembly,
+            typeof(InvoiceCalculatorService).Assembly,
+            typeof(InvoiceRepository).Assembly
+        ];
+
+        foreach (var assembly in assemblies)
+        {
+            foreach (var type in types)
+            {
+                RegisterServices(builder, assembly, type);
+            }
+        }
+
+        RegisterSpecification(builder);
+    }
+
+    private static void RegisterSpecification(ContainerBuilder builder)
+    {
+        var assembly = typeof(InvoiceCalculatorService).Assembly;
+        const string key = "a";
+
+        builder.RegisterAssemblyTypes(assembly)
+               .Where(t => t.Name.EndsWith("Specification") 
+                    && !t.IsAbstract 
+                    && t.Name != nameof(CanAddOperationSpecification))
+               .Keyed<IAddOperationSpecification>(key)
+               .AsImplementedInterfaces()
+               .InstancePerRequest();
+
+        builder.Register(ctx => new CanAddOperationSpecification(ctx.ResolveKeyed<IEnumerable<IAddOperationSpecification>>(key)))
+               .AsImplementedInterfaces()
+               .InstancePerRequest();
+    }
+
+    private static void RegisterServices(ContainerBuilder builder, Assembly assembly, string suffix)
+    {
+        builder.RegisterAssemblyTypes(assembly)
+               .Where(t => t.Name.EndsWith(suffix) && !t.IsAbstract)
+               .AsImplementedInterfaces()
+               .AsSelf()
+               .InstancePerRequest();
+    }
+}
+
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,9 +1,22 @@
+using Application.Invoices.Commands;
+using Application.Invoices.Queries;
+using Application.Operations;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+{
+    containerBuilder.RegisterServices();
+});
 
 var app = builder.Build();
 
@@ -13,12 +26,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/weatherforecast", () =>
-{
-    return "";
-})
+app.MapPost("/onvoice/calculate", (InvoiceCommandService invoiceCommandService, CalculateInvoicesCommand cmd) => 
+    invoiceCommandService.CalculateInvoices(cmd));
 
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapGet("/onvoice", (InvoiceQueryService invoiceQueryService) => invoiceQueryService.GetInvoices());
+
+app.MapPost("/operation", (OperationCommandService operationCommandService, [FromBody]AddOperationCommand cmd) =>
+{
+    operationCommandService.AddOperation(cmd);
+});
+
 
 app.Run();
