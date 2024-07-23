5edebda4582373dc5e9e1dc38e7e59fc4813c222
Ja-Rek <dto.jarek@gmail.com>
2024-07-24 00:55:08 +0200

    Move specification

---
+++ Domain/Operations/Specyfications/AddOperationSpecificationBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Operations.Specyfications;
+
+public abstract class  AddOperationSpecificationBase
+{
+    abstract public SpecificationResult IsSatisfiedBy(IEnumerable<Operation> operations, OperationType newOperationType);
+
+    protected OperationType GetLastOperationType(IEnumerable<Operation> operations) => 
+        operations.OrderBy(o => o.OperationDate)
+            .Last()
+            .OperationType;
+
+    protected string GetMessage(bool isSatisfied, string message) => 
+        isSatisfied ? message : string.Empty;
+}
--- Domain/Operations/Specyfications/IAddOperationSpecification.cs
+++ Domain/Operations/Specyfications/IAddOperationSpecification.cs
@@ -4,16 +4,3 @@ public interface IAddOperationSpecification
 {
     SpecificationResult IsSatisfiedBy(IEnumerable<Operation> operations, OperationType newOperationType);
 }
-
-public abstract class  AddOperationSpecificationBase
-{
-    abstract public SpecificationResult IsSatisfiedBy(IEnumerable<Operation> operations, OperationType newOperationType);
-
-    protected OperationType GetLastOperationType(IEnumerable<Operation> operations) => 
-        operations.OrderBy(o => o.OperationDate)
-            .Last()
-            .OperationType;
-
-    protected string GetMessage(bool isSatisfied, string message) => 
-        isSatisfied ? message : string.Empty;
-}
